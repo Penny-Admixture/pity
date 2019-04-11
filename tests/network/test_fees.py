253b195b6b542b82005bde7f6d4020da4624fc4d
Peerchemist <dx.1178@gmail.com>
2019-04-11 16:17:36 +0200

    start porting to Peercoin

--- tests/network/test_fees.py
+++ tests/network/test_fees.py
@@ -1,71 +1,9 @@
-from time import sleep, time
-
-import bit
-from bit.network.fees import get_fee, get_fee_cached, set_fee_cache_time
+from bit.network.fees import get_fee
 
 
-def test_set_fee_cache_time():
-    original = bit.network.fees.DEFAULT_CACHE_TIME
-    set_fee_cache_time(30)
-    updated = bit.network.fees.DEFAULT_CACHE_TIME
-
-    assert original != updated
-    assert updated == 30
-
-    set_fee_cache_time(original)
+def test_get_fee_smol():
+    assert get_fee(949) == 0.01
 
 
-def test_get_fee():
-    assert get_fee(fast=True) >= get_fee(fast=False)
-
-
-class TestFeeCache:
-    def test_fast(self):
-        sleep(0.2)
-
-        start_time = time()
-        set_fee_cache_time(0)
-        get_fee_cached(fast=True)
-        initial_time = time() - start_time
-
-        start_time = time()
-        set_fee_cache_time(600)
-        get_fee_cached(fast=True)
-        cached_time = time() - start_time
-
-        assert initial_time > cached_time
-
-    def test_hour(self):
-        sleep(0.2)
-
-        start_time = time()
-        set_fee_cache_time(0)
-        get_fee_cached(fast=False)
-        initial_time = time() - start_time
-
-        start_time = time()
-        set_fee_cache_time(600)
-        get_fee_cached(fast=False)
-        cached_time = time() - start_time
-
-        assert initial_time > cached_time
-
-    def test_expires(self):
-        sleep(0.2)
-
-        set_fee_cache_time(0)
-        get_fee_cached()
-
-        start_time = time()
-        set_fee_cache_time(600)
-        get_fee_cached()
-        cached_time = time() - start_time
-
-        sleep(0.2)
-
-        start_time = time()
-        set_fee_cache_time(0.1)
-        get_fee_cached()
-        update_time = time() - start_time
-
-        assert update_time > cached_time
+def test_get_fee_big():
+    assert get_fee(3903) == 0.03903
